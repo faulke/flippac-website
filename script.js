b22fd0a00aa47730e68ff83fc36c3b4820ff9d84
faulke <faulkev@gmail.com>
2016-04-01 07:49:27 -0600

    Image loading

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
     
+    var image = document.getElementById('carousel-pic-1'),
+    url = 'images/calm.jpg';
+    image.style.backgroundImage = 'images/loading.gif';
+    var tmp = new Image();
+    tmp.onload = function() {
+       image.style.backgroundImage = "url('"+url+"')";
+       // or insert some other special effect code here.
+    };
+    tmp.src = url;
+    
     //active class for nav buttons
     $("ul li").on("click", function () {
         $(this).addClass("activate");
@@ -9,7 +19,7 @@ $(document).ready(function () {
     //moves navbar to top of window on scroll down
     $(window).bind('scroll', function () {
         var navHeight = $(window).height();
-        if ($(window).scrollTop() > navHeight - 20) {
+        if ($(window).scrollTop() > navHeight - 10) {
             $('nav').addClass('navbar-fixed-top');
         } else {
             $('nav').removeClass('navbar-fixed-top');
