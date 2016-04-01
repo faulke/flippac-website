c03da40f6c035cf8635c13b39ddde551a2daf63f
faulke <faulkev@gmail.com>
2016-03-31 23:01:47 -0600

    Add scroll and button animations

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
-$(document).ready(function() {
-
-$("ul li").on("click", function(){
-   $(this).addClass("activate");
-   $(this).siblings().removeClass("activate");
-});
+$(document).ready(function () {
     
-           $(window).bind('scroll', function() {
-           var navHeight = $( window ).height();
-                 if ($(window).scrollTop() > navHeight - 40) {
-                     $('nav').addClass('navbar-fixed-top');
-                 }
-                 else {
-                     $('nav').removeClass('navbar-fixed-top');
-                 }
-            });
-
-
-/*$('a').click(function(){
-    $('html, body').animate({
-        scrollTop: $( $.attr(this, 'href') ).offset().top
-    }, 500);
-    return false;
-});*/
-});
+    //active class for nav buttons
+    $("ul li").on("click", function () {
+        $(this).addClass("activate");
+        $(this).siblings().removeClass("activate");
+    });
     
+    //moves navbar to top of window on scroll down
+    $(window).bind('scroll', function () {
+        var navHeight = $(window).height();
+        if ($(window).scrollTop() > navHeight - 40) {
+            $('nav').addClass('navbar-fixed-top');
+        } else {
+            $('nav').removeClass('navbar-fixed-top');
+        }
+    });
+
+    //slow scroll to sections
+    $('a[href*="#"]').click(function (event) {
+        $('html, body').animate({
+            scrollTop: $($.attr(this, 'href')).offset().top
+        }, 1700);
+        event.preventDefault();
+    });
+    
+    //bouncing scroll down button
+    setInterval(function () {
+        var btn = $('.btn-container');
+        btn.animate({top: "10px"});
+        btn.animate({top: "-10px"});
+        btn.animate({top: "10px"});
+        btn.animate({top: "-10px"});
+    }, 4000);
+});
