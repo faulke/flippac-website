6fd891586334d4c8e5ef1f5aa00e4ebaed7e5601
faulke <faulkev@gmail.com>
2016-10-26 23:05:47 -0600

    update for heroku

--- server.js
+++ server.js
@@ -2,16 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('public'));
 
 app.get('/home', (req, res) => {
    res.sendFile(path.resolve('views/index.html'));
-})
+});
 
 app.get('/team/evan', (req, res) => {
   res.sendFile(path.resolve('views/team/evan.html'));
-})
-
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
 });
